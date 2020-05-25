3d7854c1af634e3c05bd38ee71507222b424c652
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-25 19:18:30 +0800

    hello redirect ok

--- myapp/views.py
+++ myapp/views.py
@@ -1,43 +1,18 @@
-from myapp.models import Dreamreal
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+import datetime
 
-def crudops(request):
-   #Creating an entry
-   
-   dreamreal = Dreamreal(
-      website = "www.polo.com", mail = "sorex@polo.com", 
-      name = "sorex", phonenumber = "002376970"
-   )
-   
-   dreamreal.save()
-   
-   #Read ALL entries
-   objects = Dreamreal.objects.all()
-   res ='Printing all Dreamreal entries in the DB : <br>'
-   
-   for elt in objects:
-      res += elt.name+"<br>"
-   
-   #Read a specific entry:
-   sorex = Dreamreal.objects.get(name = "sorex")
-   res += 'Printing One entry <br>'
-   res += sorex.name
-   
-   #Delete an entry
-   res += '<br> Deleting an entry <br>'
-   sorex.delete()
-   
-   #Update
-   dreamreal = Dreamreal(
-      website = "www.polo.com", mail = "sorex@polo.com", 
-      name = "sorex", phonenumber = "002376970"
-   )
-   
-   dreamreal.save()
-   res += 'Updating entry<br>'
-   
-   dreamreal = Dreamreal.objects.get(name = 'sorex')
-   dreamreal.name = 'thierry'
-   dreamreal.save()
-   
-   return HttpResponse(res)
+# Create your views here.
+def hello(request):
+   today = datetime.datetime.now().date()
+   daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+   return redirect("https://www.djangoproject.com")
+	
+def viewArticle(request, articleId):
+   """ A view that display an article based on his ID"""
+   text = "Displaying article Number : %s" %articleId
+   return redirect(viewArticles, year = "2045", month = "02")
+	
+def viewArticles(request, year, month):
+   text = "Displaying articles of : %s/%s"%(year, month)
+   return HttpResponse(text)
