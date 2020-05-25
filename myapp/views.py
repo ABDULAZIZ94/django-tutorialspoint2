5d3cd4cd3ded3ef8b503c2bb5eaca5150688eaca
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-25 16:24:16 +0800

    datetime ok

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
 
 def hello2(request):
    text = """<h1>welcome to my app !</h1>"""
    return HttpResponse(text)
 # Create your views here.
 
-def hello(request):
+def hello3(request):
    return render(request, "hello.html", {})
+
+def hello(request):
+   today = datetime.now().date()
+   return render(request, "hello.html", {"today" : today})
+
