7ee942cffda9068706bf235ef001ea96e7fd7bfc
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-26 07:26:53 +0800

    cookies handling abandoned

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, render_to_response
 from django.http import HttpResponse
 import datetime
 from myapp.forms import LoginForm
+from django.template import RequestContext
 
 # Create your views here.
 def hello(request):
@@ -9,34 +10,37 @@ def hello(request):
    daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
    return render(request, "hello.html", {"today" : today})
 
-def viewArticle(request, articleId):
-   """ A view that display an article based on his ID"""
-   text = "Displaying article Number : %s" %articleId
-   return redirect(viewArticles, year = "2045", month = "02")
-	
-def viewArticles(request, year, month):
-   text = "Displaying articles of : %s/%s"%(year, month)
-   return HttpResponse(text)
-
-def static(request):
-   return render(request, "static.html", {})
-
 def login(request):
-   username = 'not logged in'
+   username = "not logged in"
    
-   if request.method == 'POST':
+   if request.method == "POST":
+      #Get the posted form
       MyLoginForm = LoginForm(request.POST)
-      
-      if MyLoginForm.is_valid():
-         username = MyLoginForm.cleaned_data['username']
-         request.session['username'] = username
-      else:
-         MyLoginForm = LoginForm()
-   return render(request, 'loggedin.html', {"username" : username})
+   
+   if MyLoginForm.is_valid():
+      username = MyLoginForm.cleaned_data['username']
+   else:
+      MyLoginForm = LoginForm()
+   
+   response = render_to_response(request, 'loggedin.html', {"username" : username}, context_instance = RequestContext(request))
+   
+   response.set_cookie('last_connection', datetime.datetime.now())
+   response.set_cookie('username', datetime.datetime.now())
+	
+   return response
 
 def formView(request):
-   if request.session.has_key('username'):
-      username = request.session['username']
-      return render(request, 'loggedin.html', {"username" : username})
+   if 'username' in request.COOKIES and 'last_connection' in request.COOKIES:
+      username = request.COOKIES['username']
+      
+      last_connection = request.COOKIES['last_connection']
+      last_connection_time = datetime.datetime.strptime(last_connection[:-7], 
+         "%Y-%m-%d %H:%M:%S")
+      
+      if (datetime.datetime.now() - last_connection_time).seconds < 10:
+         return render(request, 'loggedin.html', {"username" : username})
+      else:
+         return render(request, 'login.html', {})
+			
    else:
       return render(request, 'login.html', {})
